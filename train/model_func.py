47a6c1cec17ee18a1dd3dbe86a5a400f75fbfa1f
Madina Sultanova <madinaa.1467@gmail.com>
2021-08-17 00:57:34 +0600

    31 change N_WORKERS

--- train/model_func.py
+++ train/model_func.py
@@ -39,6 +39,8 @@ def run_model(
         epochs=n_epochs,
         validation_data=validation_generator,
         callbacks=callbacks,
+        steps_per_epoch=train_generator.samples // BATCH_SIZE,
+        validation_steps=validation_generator.samples // BATCH_SIZE,
         workers=n_workers  # TODO adjust this according to the number of CPU cores of your machine
     )
 
